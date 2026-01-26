87544ecfe68f2961a0665c0787e6927d236a172f
Majk_Z-IT <156111094+Mayk-ITdS@users.noreply.github.com>
2026-01-26 23:36:50 +0100

    chore(api): centralise et instancie axios api avec RequestOptions customisees

--- frontend/src/hooks/useAxiosWithAuth.ts
+++ frontend/src/hooks/useAxiosWithAuth.ts
@@ -1,15 +1,30 @@
+import { useCallback } from "react";
 import api from "../utils/api";
+import type { RequestOptions } from "@/types/requestOptions";
+const useAxiosWithAuth = () => {
+  const request = useCallback(
+    async <T>(options: RequestOptions): Promise<T> => {
+      const { method, data, params, headers, url } = options;
+      try {
+        const token = localStorage.getItem("authToken");
+        if (!token) {
+          throw new Error("No authorization token found");
+        }
+        const res = await api.request<T>({
+          url: url,
+          method,
+          data,
+          params,
+          headers: { Authorization: `Bearer ${token}`, ...headers },
+        });
 
-const useAxiosWithAuth = async () => {
-  try {
-    const token = localStorage.getItem("authToken");
-    if (!token) return;
-    const res = await api.get(`/apiBaseUrl/:id`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    console.log("Fetched data", res.data);
-  } catch (err) {
-    console.error("Error fetching data", err);
-  }
+        return res.data;
+      } catch (err) {
+        throw new Error(`${err}`);
+      }
+    },
+    []
+  );
+  return request;
 };
 export default useAxiosWithAuth;
