c7265b32dfce24aaf59eb129390789380200c95e
Majk_Z-IT <156111094+Mayk-ITdS@users.noreply.github.com>
2026-01-29 13:55:28 +0100

    feat(ui) : amélioration du shell applicatif (layout, navbar, footer)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,33 +10,33 @@ import { opinions } from "@/lib/mockData";
 
 const Home = () => {
   return (
-    <div className="min-h-screen mx-auto w-max-6xl bg-background bg-gradient-to-b from primary/5 via-background to-background">
+    <div className="min-h-screen mx-auto w-full">
       <header className="px-4 py-2"></header>
       <main className="mx-auto container md:px-0">
-        <div className="mx-2 grid gap-8 md:grid-cols-2 sx:grid-cols-1 md:items-center">
-          <div className="space-y-10 text-center items-center">
-            <Badge className="text-lg" variant="secondary">
+        <div className="mx-2 grid h-200 gap-8 md:grid-cols-2 sm:grid-cols-1 md:items-center">
+          <div className="flex flex-col space-y-10 text-center items-center">
+            <Badge className="text-2xl" variant="secondary">
               Catering • Bordeaux
             </Badge>
-            <h1 className="text-5xl font-semibold tracking-tight">
+            <h1 className="text-7xl font-semibold tracking-tight">
               Catering premium sans stress
             </h1>
-            <p className="text-muted-foreground">
+            <p className="text-muted-foreground text-xl">
               Choisis un menu, indique la date et l’adresse, notre equipe
               s’occupe du reste.
             </p>
-            <div className="flex flex-col gap-4 justify-around w-full md:flex-row">
-              <Button size={"lg"} className="text-lg">
+            <div className="flex flex-col gap-4 justify-around w-full">
+              <Button size={"lg"} className="text-2xl">
                 Découvrir les menus
               </Button>
-              <Button size={"lg"} className="text-lg">
+              <Button size={"lg"} className="text-2xl">
                 Demander un devis
               </Button>
             </div>
           </div>
-          <Card className="overflow-hidden w-3/4 m-auto w-max-200 py-10 px-5 shadow-lg backdrop-blur-xl">
+          <Card className="overflow-hidden h-150 w-3/4 py-0 m-auto w-full px-2 shadow-2xl backdrop-blur-xl">
             <CardContent className="p-0">
-              <div className="h-56 w-full bg-gradient-to-br from-primary/20 to-transparent">
+              <div className="h-auto w-full bg-gradient-to-br from-primary/20 to-transparent">
                 <div className="overflow-hidden w-full h-full">
                   <div className="carousel-track w-max flex h-full">
                     {[...demoFoodImages, ...demoFoodImages].map(
@@ -45,9 +45,9 @@ const Home = () => {
                           key={index}
                           src={src}
                           alt="food image"
-                          className="w-80 shrink-0 h-full slide mr-2 object-cover"
+                          className="w-auto shrink-0 h-full slide mr-2 object-cover"
                         />
-                      )
+                      ),
                     )}
                   </div>
                 </div>
